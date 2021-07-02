24540a46d0a5567b033a0bd0aed0a85cc78ffd06
LalithaKrishnamoorthy <lalitha.reka@gmail.com>
2021-07-02 13:26:40 +0530

    Code commited by Lalitha

---
+++ src/test/java/org/test/SampleGitProject/TestGit.java
@@ -0,0 +1,14 @@
+package org.test.SampleGitProject;
+
+public class TestGit {
+	public static void main(String[] args) {
+		String name="lalitha";
+		String address="Trichy";
+		System.out.println(name);
+		System.out.println(address);
+		
+	}
+	
+	
+
+}
