0b2bca77bf1daf69d5412fbaacbe1f0d9a2e4d8e
LalithaKrishnamoorthy <lalitha.reka@gmail.com>
2021-07-04 23:21:20 +0530

    Code commit into branch A

--- src/test/java/org/test/SampleGitProject/TestGit.java
+++ src/test/java/org/test/SampleGitProject/TestGit.java
@@ -4,8 +4,11 @@ public class TestGit {
 	public static void main(String[] args) {
 		String name="lalitha";
 		String address="Trichy";
+		String phnNo="8015349262";
 		System.out.println(name);
 		System.out.println(address);
+		System.out.println(phnNo);
+		
 		
 	}
 	
